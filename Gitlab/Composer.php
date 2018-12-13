393a01c5cea7bf114a8d832aa671775a7cea6117
Jochen Roth <j.roth@zeroseven.de>
2018-12-14 00:15:04 +0100

    Add basic class and binary

--- Gitlab/Composer.php
+++ Gitlab/Composer.php
@@ -1,1 +1,16 @@
 <?php
+
+namespace Gitlab;
+
+Class Composer {
+    /**
+     * @var string
+     */
+    protected $user = "root";
+
+
+    public function doStuff($huhu) {
+        return '>>>>>>>>> ' . $huhu;
+    }
+
+}
