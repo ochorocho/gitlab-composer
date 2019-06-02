c470307ef9f1893c9d0988c26c3cceef63cfa806
Jochen <rothjochen@gmail.com>
2019-06-02 18:33:07 +0200

    Add debugging

--- src/Console/Command/BuildLocalCommand.php
+++ src/Console/Command/BuildLocalCommand.php
@@ -147,6 +147,9 @@ EOT
                 $versionToBuild = preg_replace('/^v/', '$1', $versionToBuild);
             }
 
+
+            echo $packageVersion . " !== " . $versionToBuild . "\n";
+
 //            if($packageVersion !== $versionToBuild) {
 //              unset($packages[$key]);
 //            }
