9f64f14b997fb364d7769c7ace4ac587c8d2a5fe
Jochen <rothjochen@gmail.com>
2020-02-22 15:38:00 +0100

    Add some tests for new commands after docker build

--- tests.sh
+++ tests.sh
@@ -2,4 +2,5 @@
 
 # some commands must be executable
 composer --version || exit 1
-composer help package || exit 1
+composer help package | grep "options" || exit 1
+composer help publish | grep "<project-url>" || exit 1
